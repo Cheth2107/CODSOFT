81422993c0bcde16d54dab7af97cf4b89516068e
Cheth2107 <143086835+Cheth2107@users.noreply.github.com>
2023-10-29 23:34:59 +0530

    Update calculator.py

--- task2/calculator.py
+++ task2/calculator.py
@@ -1,3 +1,11 @@
+#CALCULATOR
+
+#Design a simple calculator with basic arithmetic operations.
+#Prompt the user to input two numbers and an operation choice.
+#Perform the calculation and display the result.
+
+
+
 import tkinter as tk
 
 def on_click(button_text):
